0a41bfac19ce4b6c9f691a0b65789fbf8b019abe
WSChange <1578632568@qq.com>
2025-12-02 14:11:07 +0800

    营业额数据统计业务功能开发

---
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,71 @@
+package com.sky.service.impl;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ReportServiceImpl implements ReportService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 统计指定时间区间内的营业额数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    public TurnoverReportVO turnoverStatistics(LocalDate begin, LocalDate end) {
+
+        // 当前集合用于存放从begin到end范围内的每天的日期
+        List<LocalDate> dateList = new ArrayList<>();
+
+        // 使用add方法将每个日期添加到集合当中
+        dateList.add(begin);
+
+        while(!begin.equals(end)){
+            // 使用循环计算指定日期的后一天对应的日期
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+
+        // 存放每天的营业额
+        List<Double> turnoverList = new ArrayList<>();
+
+        for (LocalDate date : dateList){
+            // 查询date日期对应的营业额数据
+            LocalDateTime beginTime = LocalDateTime.of(date,LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date,LocalTime.MAX);
+
+            // select sum(amount) from orders where order_time > beginTime and order_Time < endTime and status = 5
+            // 将当天的开始和结束时间存入map
+            Map map = new HashMap();
+            map.put("status", Orders.COMPLETED);
+            map.put("begin",beginTime);
+            map.put("end",endTime);
+            Double turnover = orderMapper.sumByMap(map);
+            //注意判断当前营业额为空时对数据进行处理
+            turnover = turnover == null ? 0.0 : turnover;
+            turnoverList.add(turnover);
+        }
+
+        // 封装返回结果
+        return TurnoverReportVO.builder()
+                .dateList(StringUtils.join(dateList,","))
+                .turnoverList(StringUtils.join(turnoverList,","))
+                .build();
+    }
+}
