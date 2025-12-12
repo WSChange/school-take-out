6fc7f3624a4418093b8f7d4c7856a8bd22cc0f88
WSChange <1578632568@qq.com>
2025-12-12 16:57:00 +0800

    修复菜品不能删除的bug

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -18,10 +18,10 @@ public interface DishFlavorMapper {
 
     /**
      * 根据菜品id删除对应的口味数据
-     * @param dishid
+     * @param dishId
      */
     @Delete("delete from dish_flavor where dish_id = #{dishId}")
-    void deleteByDishId(Long dishid);
+    void deleteByDishId(Long dishId);
 
     /**
      * 根据菜品id集合批量删除关联的口味数据
--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -55,11 +55,6 @@ public interface DishMapper {
     @Delete("delete from dish where id = #{id}")
     void deleteById(Long id);
 
-    /**
-     * 根据菜品id集合批量删除菜品
-     * @param ids
-     */
-    void deleteByIds(List<Long> ids);
 
     /**
      * 根据id动态修改菜品
